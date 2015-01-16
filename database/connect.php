1ec6b50a1976ea73d96c66f3cc80949645bff0be
Polycode <Polycode@laposte.net>
2015-01-16 10:06:23 +0100

    add database and forms

---
+++ database/connect.php
@@ -0,0 +1,50 @@
+<?php
+if(!(isset($_POST['login']) && isset($_POST['password'])))
+{
+    header('Location: ../loginForm.html');
+}
+else
+{
+    // On va vérifier les variables
+    if(!preg_match('/^[[:alnum:]]+$/', $_POST['login']) or !preg_match('/^[[:alnum:]]+$/', $_POST['password']))
+    {
+        echo 'Vous devez entrer uniquement des lettres ou des chiffres <br/>';
+        echo '<a href="index.php" temp_href="index.php">Réessayer</a>';
+        exit();
+    }
+    else
+    {
+        require('config.php'); // On réclame le fichier
+
+        $login = $_POST['login'];
+        $password = $_POST['password'];
+
+		require('util.php');
+		define("ENCRYPTION_KEY", "=@~#;:&-");
+		
+		$req = $bdd->prepare('SELECT login,password FROM user WHERE login=:login');
+		$req->execute(array('login'=>$login));
+		$user = $req->fetch();
+		if ($req->rowCount() > 0) {
+			//le login existe  header('Location: ../home.html');
+			$passwordBD = decrypt(base64_decode($user['password']), ENCRYPTION_KEY);
+			if($passwordBD==$password){
+			//c'est le bon password
+			echo "Vous êtes connecté";
+			header('Location: ../home.html');
+			}
+			else{
+			//mauvais password
+			echo "Erreur de mot de passe, veuillez le retaper";
+			header('Location: ../loginForm.html');
+			}
+		}
+		else{
+		//login existe pas
+		}
+		$req->closeCursor();
+		
+		
+    }
+}
+?>
